3791bf1787459335f97d88a33b7813b183f5c811
Joydeep Ghatak <joydeep.ghatak@gmail.com>
2020-03-31 17:08:47 -0400

    Added code snippets and completed url_utilities.py

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,4 +1,6 @@
 # from urllib.request import urlopen
+import re
+import string
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
@@ -36,36 +38,35 @@ def scrape_page(page_contents: str):
 	lines = (line.strip() for line in text.splitlines())
 	chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
 
+	text = ' '.join(chunk for chunk in chunks if chunk)
+	plain_text = ''.join(filter(lambda x: x in string.printable, text))
 
-text = ' '.join(chunk for chunk in chunks if chunk)
-plain_text = ''.join(filter(lambda x: x in string.printable, text))
+	clean_words = []
 
-clean_words = []
+	words = plain_text.split(" ")
+	for word in words:
+		clean = True
 
-words = plain_text.split(" ")
-for word in words:
-	clean = True
+		# no punctuation
+		for punctuation_marks in string.punctuation:
+			if punctuation_marks in word:
+				clean = False
 
-	# no punctuation
-for punc in string.punctuation:
-	if punc in word:
-		clean = False
+			# no numbers
+			if any(char.isdigit() for char in word):
+				clean = False
 
-		# no numbers
-	if any(char.isdigit() for char in word):
-		clean = false
+			# at least two characters but no more than 10
+			if len(word) < 2 or len(word) > 10:
+				clean = False
 
-		# at least two characters but no more than 10
-	if len(word) < 2 or len(word) > 10:
-		clean = False
+			if not re.match(r'^\w+$', word):
+				clean = False
 
-	if not re.match(r'^\w+$', word):
-		clean = False
+			if clean:
+				try:
+					clean_words.append(word.lower())
+				except UnicodeEncodeError:
+					print(".")
 
-	if clean:
-		try:
-			clean_words.append(word.lower())
-		except UnicodeEncodeError:
-			print(".")
-
-return clean_words
+		return clean_words
