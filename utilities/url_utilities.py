95e5a5bd6053d014bfeab4a6ff74694db58707a2
Joydeep Ghatak <joydeep.ghatak@gmail.com>
2020-03-31 15:38:52 -0400

    Fixed formatting issues.

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,17 +1,13 @@
 def load_url_from_files(file_path: str):
-	#TODO: add the code needed to read the text file with the urls in it
+	# TODO: add the code needed to read the text file with the urls in it
 	pass
 
+
 def load_page(url: str):
-	#TODO: add the code that reads the url
+	# TODO: add the code that reads the url
 	pass
 
 
 def scrape_page(page_contents: str):
-	#TODO: Analyze the text
+	# TODO: Analyze the text
 	pass
-
-
-
-
-
